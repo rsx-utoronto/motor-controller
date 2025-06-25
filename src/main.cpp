2ff12641f6320df4aa96ea1a4040e5dd225c3863
Mark Parvez <pirho60@gmail.com>
2025-06-24 22:54:49 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+#define LED_PIN PC6
+#define BUTTON_PIN PC10
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial); // Wait for USB to connect
+  Serial.println("Hello World!");
+
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  
+  digitalWrite(LED_PIN, LOW); // Turn off LED
+}
+
+void loop() {
+  // Button pressed when pin is LOW (because of INPUT_PULLUP)
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    Serial.println("Button Pressed!");
+    digitalWrite(LED_PIN, HIGH); // Turn on LED
+  } else {
+    digitalWrite(LED_PIN, LOW);  // Turn off LED
+  }
+}
